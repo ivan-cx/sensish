186d15ef79a366c8760630a051f7bb295cf6f556
Ivan Kuznetsov <i@ivn.cx>
2020-09-13 18:27:18 +0300

    infrastructure for watermarks text #2

---
+++ src/text.cpp
@@ -0,0 +1,7 @@
+#include "text.h"
+
+void watermark_init_font(const char *filename) {}
+
+void watermark_draw_text(unsigned char *texture, int width, int height,
+                         const WatermarkText &text) {}
+
---
+++ src/text.h
@@ -0,0 +1,11 @@
+
+struct WatermarkText {
+#define TEXT_LINE_SIZE 1024
+#define TEXT_LINES_COUNT 3
+  char lines[TEXT_LINES_COUNT][TEXT_LINE_SIZE];
+};
+
+void watermark_init_font(const char *filename);
+void watermark_draw_text(unsigned char *texture, int width, int height,
+                         const WatermarkText &text);
+
